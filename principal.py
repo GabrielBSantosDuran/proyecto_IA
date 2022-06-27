b83988e117d114328db6b1de4926404930c492a6
Kevin <kevincho10916038@gmail.com>
2022-06-27 01:44:41 -0400

    Implementacion del metodo 4 y 5

--- principal.py
+++ principal.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error
+from regex import F
+from sklearn import neighbors
+from sklearn.metrics import mean_squared_error, top_k_accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
@@ -23,7 +25,7 @@ print (str(n_items) + ' items')
 
 #Valoraciones
 plt.hist(df_ratings.rating,bins=8)
-plt.show()
+plt.show()      
 
 print(df_ratings.groupby(["rating"])["userId"].count())
 
@@ -52,18 +54,41 @@ print(ratings_test.shape)
 
 #Calcular matriz
 sim_matrix = 1 - sklearn.metrics.pairwise.cosine_distances(ratings)
-print(sim_matrix.shape)
+print(sim_matrix.shape) 
 plt.imshow(sim_matrix);
 plt.colorbar()
 plt.show()
 
-#recomendaciones
-#separar las filas y columnas de train y test
-sim_matrix_train = sim_matrix[0:24,0:24]
-sim_matrix_test = sim_matrix[24:30,24:30]
+#Recomendacion por Correlacion
+average_rating = pd.DataFrame(df_ratings.groupby('repoId')['rating'].mean())
+average_rating['ratingCount'] = pd.DataFrame(df_ratings.groupby('repoId')['rating'].count())
+average_rating.sort_values('ratingCount', ascending=False).head()
 
-users_predictions = sim_matrix_train.dot(ratings_train) / np.array([np.abs(sim_matrix_train).sum(axis=1)]).T
-plt.rcParams['figure.figsize'] = (20.0, 5.0)
-plt.imshow(users_predictions);
-plt.colorbar()
+mi_repo_ratings = df_matrix[4]
+similar_to_mine = df_matrix.corrwith(mi_repo_ratings)
+corr_mine = pd.DataFrame(similar_to_mine, columns=['pearsonR'])
+corr_mine.dropna(inplace=True)
+print(corr_mine)
+corr_summary = corr_mine.join(average_rating['rating'])
+corr_summary[corr_summary['rating']>=1].sort_values('pearsonR', ascending=False).head(10)
+print(corr_summary)
+df_repos[df_repos['repoId'] == 92]
+print(df_repos)
+
+#Repo mas popular
+popular_repo=df_repos[['title','stars']].groupby('stars').sum().reset_index()
+popular_repo_top_20 = popular_repo.sort_values('stars',ascending=False).head(n=10)
+print(popular_repo)
+print(popular_repo_top_20)
+
+plt.rcdefaults()
+plt.show()
+
+objects = (list(popular_repo_top_20['title']))
+y_pos = np.arange(len(objects))
+performance =list(popular_repo_top_20['stars'])
+plt.bar(y_pos, performance, align='center',alpha=0.5)
+plt.xticks(y_pos,objects,rotation='vertical')
+plt.ylabel('Conteo de estrellas')
+plt.title('Repo mas popular')
 plt.show()
