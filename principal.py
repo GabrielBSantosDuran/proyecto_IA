38065f9345deec9b44a797d4b10e6f6da14cdfe2
MrFrost14 <gabriel_bodomir@hotmail.com>
2022-06-27 02:10:02 -0400

    Codigo terminado

--- principal.py
+++ principal.py
@@ -1,7 +1,6 @@
 from re import L
 import pandas as pd
 import numpy as np
-from regex import F
 from sklearn import neighbors
 from sklearn.metrics import mean_squared_error, top_k_accuracy_score
 from sklearn.model_selection import train_test_split
@@ -54,6 +53,65 @@ print(ratings_train.shape)
 print(ratings_test.shape)
 #Calcular matriz
 sim_matrix = 1 - sklearn.metrics.pairwise.cosine_distances(ratings)
-print(sim_matrix.shape) 
+print(sim_matrix.shape)
 plt.imshow(sim_matrix);
 plt.colorbar()
+plt.show()
+
+#recomendaciones
+#separar las filas y columnas de train y test
+sim_matrix_train = sim_matrix[0:5,0:5]
+sim_matrix_test = sim_matrix[5:7,5:7]
+
+users_predictions = sim_matrix_train.dot(ratings_train) / np.array([np.abs(sim_matrix_train).sum(axis=1)]).T
+plt.rcParams['figure.figsize'] = (20.0, 5.0)
+plt.imshow(users_predictions);
+plt.colorbar()
+plt.show()
+
+#ejemplo
+
+USUARIO_EJEMPLO = 'Sakenita' # debe existir en nuestro dataset de train!
+data = df_users[df_users['username'] == USUARIO_EJEMPLO]
+usuario_ver = data.iloc[0]['userId'] -1 # resta 1 para obtener el index de pandas
+user0=users_predictions.argsort()[usuario_ver]
+
+# Veamos los tres recomendados con mayor puntaje en la predic para este usuario
+for i, aRepo in enumerate(user0[-3:]):
+    selRepo = df_repos[df_repos['repoId']==(aRepo+1)]
+    print(selRepo['title'] , 'puntaje:', users_predictions[usuario_ver][aRepo])
+    
+
+#Recomendacion por Correlacion
+average_rating = pd.DataFrame(df_ratings.groupby('repoId')['rating'].mean())
+average_rating['ratingCount'] = pd.DataFrame(df_ratings.groupby('repoId')['rating'].count())
+average_rating.sort_values('ratingCount', ascending=False).head()
+
+mi_repo_ratings = df_matrix[4]
+similar_to_mine = df_matrix.corrwith(mi_repo_ratings)
+corr_mine = pd.DataFrame(similar_to_mine, columns=['pearsonR'])
+corr_mine.dropna(inplace=True)
+print(corr_mine)
+corr_summary = corr_mine.join(average_rating['rating'])
+corr_summary[corr_summary['rating']>=1].sort_values('pearsonR', ascending=False).head(10)
+print(corr_summary)
+df_repos[df_repos['repoId'] == 92]
+print(df_repos)
+
+#Repo mas popular
+popular_repo=df_repos[['title','stars']].groupby('stars').sum().reset_index()
+popular_repo_top_20 = popular_repo.sort_values('stars',ascending=False).head(n=10)
+print(popular_repo)
+print(popular_repo_top_20)
+
+plt.rcdefaults()
+plt.show()
+
+objects = (list(popular_repo_top_20['title']))
+y_pos = np.arange(len(objects))
+performance =list(popular_repo_top_20['stars'])
+plt.bar(y_pos, performance, align='center',alpha=0.5)
+plt.xticks(y_pos,objects,rotation='vertical')
+plt.ylabel('Conteo de estrellas')
+plt.title('Repo mas popular')
+plt.show()
